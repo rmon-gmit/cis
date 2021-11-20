7affaaf692d226e39a5a5c0eabb877e5352a3e64
Ross Monaghan <rossmono@gmail.com>
2021-11-20 23:26:49 +0000

    first commit

---
+++ src/main/java/com/rmon/ci_system/CiSystemApplication.java
@@ -0,0 +1,13 @@
+package com.rmon.ci_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rmon/ci_system/CiSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rmon.ci_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
